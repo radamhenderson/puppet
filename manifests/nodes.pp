26c8c2540bd88ab0b1a37173a86c95749711ee8e
root <root@ip-10-98-79-249.ec2.internal>
2014-01-03 13:39:31 -0500

    pupet config using p/c/r pattern

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,3 +1,17 @@
 node default {
 
 }
+
+node 'www.potluckadventures.com' {
+include potluckadventures
+}
+node 'www.radamhenderson.com' {
+
+
+class { 'wordpress':
+  wordpress_db_name    => 'wordpress',
+  wordpress_db_user     => 'wordpress',
+  wordpress_db_password => 'hvyHa23rnjn%46',
+}
+
+}
